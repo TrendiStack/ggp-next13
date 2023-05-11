5f63eaca6a3b4508b81bf63709bce0dd6908f16c
Terel <terel.phillips23@gmail.com>
2023-05-11 12:31:43 -0400

    -Added pizzeria to footer

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -14,7 +14,7 @@ const Footer = () => {
       ${pathname === '/' ? '' : 'max-md:mt-32'}
       flex flex-col md:flex-row max-md:flex-col-reverse justify-between items-center gap-1 md:gap-0 w-full py-2 text-white text-xl 2xl:text-lg px-[5%] lg:px-[2%]`}
     >
-      <p>© Gelato Gelato / All Rights Reserved</p>
+      <p>© Gelato Gelato Pizzeria / All Rights Reserved</p>
 
       <MenuFooter />
     </footer>
