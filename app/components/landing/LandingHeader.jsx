5c3ea666dc0ed27b049fb900af45753fe0c5a1b0
Terel <terel.phillips23@gmail.com>
2023-06-30 11:33:37 -0400

    -animation now starts with a y trnslate of 100

--- app/components/landing/LandingHeader.jsx
+++ app/components/landing/LandingHeader.jsx
@@ -100,22 +100,26 @@ const LandingHeader = () => {
       <div className="absolute top-0 left-0 bg-black opacity-40 h-full w-full"></div>
       <div className="w-full text-white text-center absolute top-1/2 -translate-y-1/2">
         <div className="text-[23vw] md:text-[130px] leading-none lg:leading-tight max-md:mb-10">
-          <h1 className="overflow-hidden">
-            <span id="create" className="inline-block">
+          <h1 aria-label="GELATO GELATO" className="overflow-hidden">
+            <span id="create" className="translate-y-[100%] inline-block">
               GELATO GELATO
             </span>
           </h1>
-          <h2 className="overflow-hidden">
+          <h2 aria-label="PIZZERIA" className="overflow-hidden">
             <span
               id="create"
-              className="max-xl:text-[88%] xl:tracking-[1.25rem] inline-block"
+              className="max-xl:text-[88%] xl:tracking-[1.25rem] translate-y-[100%] inline-block"
             >
               PIZZERIA
             </span>
           </h2>
         </div>
         <div className="overflow-hidden">
-          <p id="create">
+          <p
+            aria-label="3650 Langstaff Rd Woodbridge"
+            id="create"
+            className="translate-y-[100%]"
+          >
             <span className={`${mulish.className}`}>3650</span> Langstaff Rd
             Woodbridge
           </p>
@@ -124,7 +128,7 @@ const LandingHeader = () => {
 
       <div
         id="head-icons"
-        className="flex gap-4 absolute bottom-5 left-[5%] lg:left-[2%]"
+        className="flex gap-4 absolute bottom-5 left-[5%] lg:left-[2%] opacity-0"
       >
         <DeliveryIcon
           href="https://order.online/store/gelato-gelato-pizzeria-vaughan-2415193/?hideModal=true&pickup=true"
@@ -143,7 +147,7 @@ const LandingHeader = () => {
         />
       </div>
       <div className="overflow-hidden text-sm md:text-base absolute bottom-7 xl:bottom-5 right-[5%] md:right-[2%] xl:right-1/2 xl:translate-x-1/2 text-white font-light">
-        <div id="create" className="inline-block">
+        <div id="create" className="translate-y-[100%] inline-block">
           <Link href="/menu">MENU / </Link>
           <Link href="/order-cake">GELATO CAKES</Link>
         </div>
