8ad26dea127a17447daf1e61cdd56181e2736647
Terel <terel.phillips23@gmail.com>
2023-06-30 10:35:56 -0400

    -responsive changes to video component text

--- app/components/landing/Video.jsx
+++ app/components/landing/Video.jsx
@@ -4,7 +4,7 @@ const Video = () => {
       aria-label="Sicilian Flavors Advertisement"
       className="grid grid-cols-1 md:grid-cols-2 gap-14 md:gap-0 lg:h-screen"
     >
-      <div className="text-center large-text lg:mx-5 2xl:mx-20 pt-5 md:h-full container mx-auto">
+      <div className="text-center large-text lg:px-5 2xl:px-20 max-md:pt-5 md:h-full container mx-auto">
         <p
           aria-label="Sicilian Flavors Description"
           className="relative top-1/2 -translate-y-1/2"
