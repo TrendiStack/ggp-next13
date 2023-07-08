d91b38b978593496ec987df8c814d5a8f76040d6
Terel <terel.phillips23@gmail.com>
2023-07-08 14:26:12 -0400

    -maintanence

--- app/order-cake/page.jsx
+++ app/order-cake/page.jsx
@@ -11,12 +11,16 @@ const page = () => {
     <main aria-label="Order Cake Page" className="bg-primary">
       <Header route="Order a Gelato Cake" />
       <section aria-label="Cake builder">
-        <h1 className="text-center text-3xl md:text-5xl text-white mt-20">
+        {/* <h1 className="text-center text-3xl md:text-5xl text-white mt-20">
           Customize your Cake
+        </h1> */}
+        <h1 className="text-center text-3xl md:text-5xl text-white mt-20">
+          Down for maintenance. Please check back later. Thank you for your
+          patience.
         </h1>
         <div className="grid grid-cols-1 lg:grid-cols-2 gap-10 w-full mx-auto lg:max-w-[900px] 2xl:max-w-[1200px] mt-32">
-          <CakeImage />
-          <Checkout />
+          {/* <CakeImage />
+          <Checkout /> */}
         </div>
       </section>
     </main>
